8cbbd081f3849afdf0f0cb8c7157b8ad968aa18d
Nadav Nehoran <nn233@cornell.edu>
2015-06-13 22:32:49 -0700

    adding autosnap

---
+++ auto_snap.py
@@ -0,0 +1,79 @@
+import urllib2
+import json
+import csv
+import map_interface
+import time
+import math
+
+photos_snapped = []
+old_lat = 0
+old_lon = 0
+
+# query a url and return the result in JSON format
+def query(url):
+    json_string = urllib2.urlopen(url).read()
+    return json.loads(json_string)
+
+# read the vehicle data from the simulator
+def readVehicleData():
+    url = 'http://172.31.99.2/vehicle'
+    return query(url)
+
+# returns the direction of the item at (lat,lon) from loction (my_lat,my_lon)
+# direction is measured counterclockwise with 0 degrees defined as straight ahead
+def getDirection(my_lat, my_lon, lat, lon):
+    dy = lat - my_lat
+    dx = lon - my_lon
+    angle = math.atan2(dy, dx)*180/math.pi - 90
+    if angle <= -180:
+        angle += 360
+    return angle
+
+# snap a photo
+def snapPhoto(title, direction, lat, lon, timestamp):
+    photos_snapped.append(title)
+    print title + ": " + str(floor(direction)) + " deg, " + str(timestamp)
+    # do something here
+    return
+
+# , read the vehicle data, get calculate
+#potential photo opportunities, and snap the photos
+def main():
+    rad = 1000
+
+    #read the user category prefereneces
+    cat = ""
+    with open('place_preferences.txt', 'rb') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            cat += ','.join(row)
+
+    while(True):
+        vd = readVehicleData()
+        lat = vd.get('GPS_Latitude')
+        lon = vd.get('GPS_Longitude')
+        ts = vd.get('Timestamp')
+
+        # calculate the direction of travel
+        car_dir = getDirection(lat, lon, old_lat, old_lon)
+        old_lat = lat
+        old_lon = lon
+
+        # find any good photo opportunities
+        places = map_interface.getPlacesNear(lat, lon, rad, cat)
+
+        # find the closest new photo candidate and snap the photo
+        for i in places:
+            title = i.get('title')
+            pos = i.get('position')
+
+            # calculate the direction of the photo
+            photo_dir = getDirection(lat, lon, pos[0], pos[1]) - car_dir
+            if not title in photos_snapped:
+                snapPhoto(title, photo_dir, lat, lon, ts)
+                break
+
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
--- map_interface.py
+++ map_interface.py
@@ -2,24 +2,26 @@ import urllib2
 import json
 import csv
 
-app_id = 'eRi9NLd3fZJeHOH2we6g'
-app_code = 'I2perok5rTYVAbGvJ3DFgg'
+app_id = '6FKzHJURtpUBesE9M9Nr'
+app_code = 'tKQv3soSelCweFMxakZNkg'
 base_url = 'http://places.cit.api.here.com/places/v1/discover/around'
 latitude = 40.74917
 longitude = -73.98529
 
 
-
+# query a url and return the result in JSON format
 def query(url):
     json_string = urllib2.urlopen(url).read()
     return json.loads(json_string)
 
+# query HERE maps for places given GPS coordinates, a radius, and a comma-separated list of categories
 def queryLocation(lat, lon, rad, cat):
     at_str =  str(lat) + ',' + str(lon)
     in_str = at_str + ';r=' + str(rad)
     url = base_url + '?in=' + in_str + '&cat=' + cat + '&app_id=' + app_id + '&app_code=' + app_code + '&tf=plain&pretty=true'
     return query(url)
     
+# parse the given JSON and extract the place items from it
 def getItems(json_object):
     j = json_object
     items = None
@@ -29,15 +31,9 @@ def getItems(json_object):
             items = results.get('items')
     elif 'items' in j:
         items = j.get('items')
-    result_list = []
-    for i in items:
-        title = i.get('title')
-        item_id = i.get('category').get('id')
-        dist = i.get('distance')
-        rating = i.get('averageRating')
-        result_list.append([title, item_id, dist])
-    return result_list
+    return items
     
+# check if there is a next page, and if so, return its URL
 def getNext(json_object):
     j = json_object
     next = None
@@ -49,19 +45,25 @@ def getNext(json_object):
         next = j.get('next')
     return next
 
+# query HERE maps for place date near a location, and parse through all of the
+# pages of data, compiling it together
 def getPlacesNear(lat, lon, rad, cat):
     query_result = queryLocation(lat, lon, rad, cat)
+#    print query_result
+ #   print "\n\n\n\n\n"
     result = getItems(query_result)
-    next = getNext(query_result)
-    while next != None:
-        query_result = query(next)
-        result += getItems(query_result)
-        print "hit"
-        next = getNext(query_result)
+#    print result
+#    next = getNext(query_result)
+#    while next != None:
+#        query_result = query(next)
+#        result += getItems(query_result)
+#        print "hit"
+#        next = getNext(query_result)
     return result
 
 def main():
 
+    
     max_rad = 150
     cat = ""
 
