90be6e8b1c448452e2f3b4b87e9d522432bdacff
Nadav Nehoran <nn233@cornell.edu>
2015-06-13 23:31:26 -0700

    fixed auto_snap

--- auto_snap.py
+++ auto_snap.py
@@ -19,9 +19,8 @@ def readVehicleData(vehicle_num):
     url = 'http://172.31.99.' + str(vehicle_num) + '/vehicle'
     return query(url)
 
-# returns the direction of the item at (lat,lon) from loction (my_lat,my_lon)
-# direction is measured counterclockwise with 0 degrees defined as straight ahead
-def getDirection(my_lat, my_lon, lat, lon):
+# returns the global direction of the item at (lat,lon) from loction (my_lat,my_lon)
+def getGlobalDir(my_lat, my_lon, lat, lon):
     dy = lat - my_lat
     dx = lon - my_lon
     angle = math.atan2(dy, dx)*180/math.pi - 90
@@ -29,17 +28,31 @@ def getDirection(my_lat, my_lon, lat, lon):
         angle += 360
     return angle
 
+# returns the relative direction of dir1 relative to dir2
+def getRelDir(dir1, dir2):
+    diff = dir1 - dir2
+    if diff < -180:
+        diff += 360
+    elif diff >= 180:
+        diff -= 360
+    return diff
+
 # snap a photo
 def snapPhoto(title, direction, lat, lon, timestamp):
     photos_snapped.append(title)
-    print title + ": " + str(floor(direction)) + " deg, " + str(timestamp)
+    print title + ": " + str(math.floor(direction)) + " deg, " + str(timestamp)
     # do something here
     return
 
 # , read the vehicle data, get calculate
 #potential photo opportunities, and snap the photos
 def main():
-    rad = 1000
+    global old_lat
+    global old_lon
+    global places
+
+    # the search radius for potential photo targets
+    rad = 150
 
     #read the user category prefereneces
     cat = ""
@@ -55,7 +68,7 @@ def main():
         ts = vd.get('Timestamp')
 
         # calculate the direction of travel
-        car_dir = getDirection(old_lat, old_lon, lat, lon)
+        car_glob_dir = getGlobalDir(old_lat, old_lon, lat, lon)
         old_lat = lat
         old_lon = lon
 
@@ -67,10 +80,14 @@ def main():
             title = i.get('title')
             pos = i.get('position')
 
-            # calculate the direction of the photo
-            photo_dir = getDirection(lat, lon, pos[0], pos[1]) - car_dir
+            # calculate the global direction of the photo
+            photo_glob_dir = getGlobalDir(lat, lon, pos[0], pos[1])
+
+            # calculate the dirction relative to the car
+            photo_rel_dir = getRelDir(photo_glob_dir, car_glob_dir)
+
             if not title in photos_snapped:
-                snapPhoto(title, photo_dir, lat, lon, ts)
+                snapPhoto(title, photo_rel_dir, lat, lon, ts)
                 break
 
         time.sleep(1)
